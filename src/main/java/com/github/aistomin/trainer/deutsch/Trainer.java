1af9946f48c383636e9ada90c2d67265c5e868fa
aistomin <andrej.istomin.ikeen@gmail.com>
2021-06-29 16:14:06 +0200

    Issue #13. Runnable class is created.

---
+++ src/main/java/com/github/aistomin/trainer/deutsch/Trainer.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2021, Istomin Andrei
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.aistomin.trainer.deutsch;
+
+import com.github.aistomin.testist.Question;
+import com.github.aistomin.testist.demo.SimpleTestConsole;
+import com.github.aistomin.testist.simple.SimpleTest;
+import java.util.ArrayList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The application entry point.
+ *
+ * @since 1.0
+ */
+public final class Trainer {
+
+    /**
+     * Logger.
+     */
+    private static final Logger LOG =
+        LoggerFactory.getLogger(Trainer.class);
+
+    /**
+     * Ctor.
+     */
+    private Trainer() {
+    }
+
+    /**
+     * Runnable method.
+     *
+     * @param args Arguments.
+     */
+    public static void main(final String... args) {
+        new SimpleTestConsole(
+            new SimpleTest(Trainer::questions),
+            Trainer.LOG::info
+        ).runTest();
+    }
+
+    /**
+     * Load the questions.
+     *
+     * @todo: Issue-17. Let's implement the method and remove the todo.
+     * @return The list of questions
+     */
+    private static List<Question> questions() {
+        return new ArrayList<>(0);
+    }
+}
