dd748a18d79d0ef75f02bdc7470dd9e8b6774199
aistomin <andrej.istomin.ikeen@gmail.com>
2021-08-12 17:18:20 +0200

    Issue #62. Implement com.github.aistomin.trainer.examinations.Exam

---
+++ src/test/java/com/github/aistomin/trainer/examinations/JsonExamTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2021, Istomin Andrei
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.aistomin.trainer.examinations;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test for {@link JsonExam}.
+ *
+ * @since 1.0
+ */
+final class JsonExamTest {
+
+    /**
+     * Delete it when we have at least one other test in this file.
+     */
+    @Test
+    void testDummy() {
+        Assertions.assertNotNull(new JsonExam());
+    }
+}
