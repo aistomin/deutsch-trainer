a186835ab8dfe60891522ff5ea057d8c3b37ac63
aistomin <andrej.istomin.ikeen@gmail.com>
2023-02-23 12:25:45 +0100

    Issue #509. Rough UI structure is created.

---
+++ src/features/dictionary/Vocabulary.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Vocabulary = () => {
+    return (
+        <section>
+            <h2>Vocabulary</h2>
+        </section>
+    );
+};
+
+export default Vocabulary;
